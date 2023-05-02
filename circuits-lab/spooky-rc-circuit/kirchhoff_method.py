a6f283da68da40db8eeae41177144293322127c7
Vincent Edwards <mister.man489@gmail.com>
2023-05-01 22:49:05 -0700

    kirchhoff method code reformatted

--- circuits-lab/spooky-rc-circuit/kirchhoff_method.py
+++ circuits-lab/spooky-rc-circuit/kirchhoff_method.py
@@ -13,18 +13,11 @@ q = sp.Function("q")
 t = sp.symbols("t")
 
 # Solve system of equations
-soln = sp.solve([
-                    i_1 - i_2 - i_3,
-                    E - i_1 * R_1 - i_2 * R_2,
-                    E - i_1 * R_1 - i_3 * R_3 - q(t) / C
-                ],
-                [
-                    i_1,
-                    i_2,
-                    i_3
-                ],
-                dict = True
-               )
+soln = sp.solve([i_1 - i_2 - i_3,
+                 E - i_1 * R_1 - i_2 * R_2,
+                 E - i_1 * R_1 - i_3 * R_3 - q(t) / C],
+                [i_1, i_2, i_3],
+                dict = True)
 
 # Solve the DE for i_3
 result = sp.dsolve(soln[0][i_3] - sp.Derivative(q(t), t), ics = {q(0): 0})
